6f885ce4b809e49d42948f4665e47f4a14eca8f1
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-11-24 15:37:27 +0100

    171717

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,34 @@
+import css from './Statistics.module.css';
+
+export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <ul className={css.list}>
+      <li className={css.item}>
+        <p className={css.text}>
+          Good: <span className={css.value}> {good}</span>
+        </p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>
+          Neutral: <span className={css.value}> {neutral}</span>
+        </p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>
+          Bad: <span className={css.value}> {bad}</span>
+        </p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>
+          Total: <span className={css.value}>{total}</span>
+        </p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>
+          Positive feedback:
+          <span className={css.value}> {positivePercentage} </span>
+        </p>
+      </li>
+    </ul>
+  );
+};
